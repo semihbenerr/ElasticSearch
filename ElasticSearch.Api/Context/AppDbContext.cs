07e8b68760551f97e184af1c7462b0e4e971b1ed
Semih BENER <benersemih2434@gmail.com>
2024-10-11 10:48:18 +0300

    Veri tabanı oluşturma

---
+++ ElasticSearch.Api/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ElasticSearch.Api.Context
+{
+    public sealed class AppDbContext : DbContext
+    //Sealed (mühürlü), sınıfların kalıtım işlemini engellemek için kullanılan bir anahtar kelimedir. Sealed olarak tanımlanan bir sınıftan kalıtım almaya çalışırsak hata verecektir. Sealed anahtar kelimesi bir sınıf için uygulanacak ise kalıtımı, bir üye için uygulanacak ise üyenin override edilmesini engellemektedir. 
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\local;Database=ElasticSearch;Trusted_Connection=True;Connect Timeout=30;MultipleActiveResultSets=True;");
+        }
+        public DbSet<Travel> Travels { get; set; }
+    }
+}
---
+++ ElasticSearch.Api/Context/Travel.cs
@@ -0,0 +1,9 @@
+﻿namespace ElasticSearch.Api.Context
+{
+    public sealed class Travel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}

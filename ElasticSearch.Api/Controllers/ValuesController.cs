732bf7d6eea87806f1e740cef93b41c6ec3da220
Semih BENER <benersemih2434@gmail.com>
2024-10-18 16:55:22 +0300

    Elastic search ekleme

--- ElasticSearch.Api/Controllers/ValuesController.cs
+++ ElasticSearch.Api/Controllers/ValuesController.cs
@@ -1,7 +1,10 @@
-﻿using ElasticSearch.Api.Context;
+﻿using Elasticsearch.Net;
+using ElasticSearch.Api.Context;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using System.Runtime.InteropServices.JavaScript;
 
 namespace ElasticSearch.Api.Controllers
 {
@@ -39,16 +42,73 @@ namespace ElasticSearch.Api.Controllers
             return Ok();
         }
         [HttpGet("[action]/{description}")]
-        public async Task<IActionResult> GetDataList(string description)
+        public async Task<IActionResult> GetDataListWithEf(string description)
         {
             IList<Travel> travels = await context.Set<Travel>().Where(d => d.Description.Contains(description)).ToListAsync();
             return Ok(travels.Take(10));
         }
         [HttpGet("[action]/{description}")]
-        public async Task<IActionResult> GetDataListAsNoTracking(string description)
+        public async Task<IActionResult> GetDataListWithEfAndAsNoTracking(string description)
         {
             IList<Travel> travels = await context.Set<Travel>().Where(d => d.Description.Contains(description)).AsNoTracking().ToListAsync();//Tek farkı .AsNoTracking() olması bir iki saniye fark ettiğini göstermek için
             return Ok(travels.Take(10));
         }
+        [HttpGet("[action]")]
+        public async Task<IActionResult> SyncToElastic()
+        {
+            var settings = new ConnectionConfiguration(new Uri("http://localhost:9200"));
+            var client = new ElasticLowLevelClient(settings);
+            List<Travel> travels = await context.Travels.ToListAsync();
+            var tasks = new List<Task>();
+            foreach (var item in travels)
+            {
+                tasks.Add(client.IndexAsync<StringResponse>("travels", item.Id.ToString(), PostData.Serializable(new
+                {
+                    item.Id,
+                    item.Title,
+                    item.Description
+                })));
+            }
+            await Task.WhenAll(tasks);
+            return Ok();
+        }
+        [HttpGet("[action]/{description}")]
+        public async Task<IActionResult> GetDataListWithElasticSearch(string description)
+        {
+            var settings = new ConnectionConfiguration(new Uri("http://localhost:9200"));
+            var client = new ElasticLowLevelClient(settings);
+            var response = await client.SearchAsync<StringResponse>("travels", PostData.Serializable(new
+            {
+                query=new
+                {
+                    wildcard = new
+                    {
+                        Description=new {value=$"*{description}*"}
+                    }
+                }
+            }));
+            var results = JObject.Parse(response.Body);
+            var hits = results["hits"]["hits"].ToObject<List<JObject>>();
+            List<Travel> travels = new List<Travel>();
+            foreach (var hit in hits)
+            {
+                travels.Add(hit["_source"].ToObject<Travel>());
+            }
+            return Ok(travels.Take(20));
+        }
+
+
+
+        #region Veri Tabanı Indexleme Info
+
+        /*
+         Veri tabanımızda seçili dbdeki tablolara indexleme ekledik bunun için tableın altındaki açılan klasörlerde index add diyip işlem yaptık
+        Yapılan işlem ;
+            1-) Indexex -> New Index -> Non Clustered Index
+            2-) Index key columns -> Add -> Id
+            3-) Index Incloude columns -> Title,Description, ...
+         */
+
+        #endregion
     }
 }
